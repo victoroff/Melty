0577ba853af38f4ba975ee9a9784a14ca6108eb6
Bebbo <mrbebz@gmail.com>
2019-07-06 13:26:22 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500*Time.deltaTime,0,0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = 25.0f;
+    private float tileLength = 15f;
+    private int amtTilesOnScreen = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        spawnTile();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z > (spawnZ - amtTilesOnScreen * tileLength))
+        {
+            spawnTile();
+        }
+    }
+
+    void spawnTile(int prefabIndex = -1) {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0] as GameObject);
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+    }
+}
