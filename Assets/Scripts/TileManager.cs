7540314501b5964df4269a1ebaa955dc374bb8a0
Bebbo <mrbebz@gmail.com>
2019-07-12 00:58:51 +0300

    removing passed tiles

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class TileManager : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     private Transform playerTransform;
-    private float spawnZ = 25.0f;
-    private float tileLength = 15f;
-    private int amtTilesOnScreen = 2;
+    private float spawnZ = 33.0f;
+    private float tileLength = 3f;
+    private float safeZone = 3f;
+    private int amtTilesOnScreen = 3;
+    private List<GameObject> activeTiles; 
+
     // Start is called before the first frame update
     void Start()
     {
+        activeTiles = new List<GameObject>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         spawnTile();
     }
@@ -19,9 +23,10 @@ public class TileManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerTransform.position.z > (spawnZ - amtTilesOnScreen * tileLength))
+        if (playerTransform.position.z - safeZone > (spawnZ - amtTilesOnScreen * tileLength))
         {
             spawnTile();
+            deleteTile();
         }
     }
 
@@ -31,5 +36,12 @@ public class TileManager : MonoBehaviour
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ;
         spawnZ += tileLength;
+        activeTiles.Add(go);
+    }
+    private void deleteTile()
+    {
+        // remove tiles behind the player
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
     }
 }
