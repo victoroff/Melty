f3157b82cb787ffe031c8ff047261f723265f220
Bebbo <mrbebz@gmail.com>
2019-07-14 16:38:19 +0300

    spawn blocks update 4

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject blockPrefab;
+    public Transform player;
+
+    //time multiplayer for each wave
+    public float WaveTime = 1f;
+    // time to spond each block
+    private float spawnTime = 2f;
+    // Spawn Blocks By time. Random on 3 spawn
+    void Update()
+    {
+        if (Time.time >= spawnTime)
+        {
+            SpawnBlocks();
+            spawnTime = Time.time + WaveTime;
+        }
+        
+    }
+
+    private void SpawnBlocks()
+    {
+        int randIdx = Random.Range(0, spawnPoints.Length);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randIdx != i)
+            {
+                var blockPosition = spawnPoints[i].position;
+                blockPosition.z = blockPosition.z + player.position.z;
+
+                Instantiate(blockPrefab, blockPosition, Quaternion.identity);
+            }
+        }
+    }
+}
