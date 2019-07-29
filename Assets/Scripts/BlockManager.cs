2b1c820c375353812e91b1ffa305eb3f8431a68c
Bebbo <mrbebz@gmail.com>
2019-07-29 17:05:46 +0300

    Update 9
    - remove obstacle (ice blocks) after passing by with player
    - wait after hitting  an obstacle

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -7,21 +7,44 @@ public class BlockManager : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject blockPrefab;
     public Transform player;
+
+    // checking if player is moving
+    public PlayerMovement movement;
     //time multiplayer for each wave
     public float WaveTime = 5f;
     // time to spond each block
     private float spawnTime = 3f;
+    private List<GameObject> activeBlocks;
 
+    private void Start()
+    {
+        activeBlocks = new List<GameObject>();
+    }
     // Spawn Blocks By time. Random on 3 spawn
-    void Update()
+    void FixedUpdate()
     {
         // when u hit a block and restart time.time is more than 0 ; reset time or put some screen, live counts?
         if (Time.timeSinceLevelLoad >= spawnTime)
         {
             SpawnBlocks();
             spawnTime = Time.time + WaveTime;
+            
         }
-        
+
+        //need to destroy the ice block after passing it
+        if (activeBlocks.Count > 0 && movement.enabled)
+        {
+           // Debug.Log(activeBlocks.Count);
+            var currentBlock = activeBlocks[0];
+
+            if (player.position.z > currentBlock.transform.position.z)
+            {
+                destroyBlocks();
+            }
+        }
+
+
+
     }
 
     private void SpawnBlocks()
@@ -32,10 +55,22 @@ public class BlockManager : MonoBehaviour
            // Debug.Log("randIDx:" + randIdx + "i:" + i);
             if (randIdx != i)
             {
+                GameObject block;
                 var blockPosition = spawnPoints[i].position;
                 blockPosition.z += player.position.z;
-                Instantiate(blockPrefab, blockPosition, Quaternion.identity);
+                block = Instantiate(blockPrefab, blockPosition, Quaternion.identity);
+                activeBlocks.Add(block);
             }
         }
+    }
+    private void destroyBlocks()
+    {
+        // remove tiles behind the player
+        for (int i = 0; i < activeBlocks.Count; i++)
+        {
+            Destroy(activeBlocks[i]);
+            activeBlocks.RemoveAt(i);
+        }
+        
     }
 }
